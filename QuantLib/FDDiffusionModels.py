6bd25186b0bbdccbd1a4bbc3ae8d98b53bdbca09
Jon Vinson <jdv@jdvinson.com>
2019-09-07 17:58:33 -0400

    Created SABRModel class

--- QuantLib/FDDiffusionModels.py
+++ QuantLib/FDDiffusionModels.py
@@ -2,8 +2,7 @@ import numpy as np
 
 class DiffusionModel:
 
-    def __init__(self, parameters, bounds, n_points):
-        self.parameters = parameters
+    def __init__(self, bounds, n_points):
         self.nx = n_points[0]
         a = bounds[0]
         b = bounds[1]
@@ -12,3 +11,33 @@ class DiffusionModel:
         a = bounds[2]
         b = bounds[3]
         self.y = np.linspace(a, b, self.ny)
+
+class SABRModel(DiffusionModel):
+
+    def Calculate(p):
+
+        alpha = p[0]
+        beta = p[1]
+        rho = p[2]
+ 
+        if self.F is None:
+            self.F = self.x[:, np.newaxis]
+
+        F = self.F
+        sig = self.y
+        nx = self.nx
+        ny = self.ny
+
+        if self.ss is None:
+            self.ss = np.zeros((nx, ny, 2, 2))  
+
+        ss = self.ss
+    
+        fb = np.power(F, beta)
+
+        ss[:, :, 0, 0] = (sig * fb) ** 2
+        ss[:, :, 1, 0] = alpha * rho * (sig ** 2) * fb
+        ss[:, :, 0, 1] = ss[:, :, 1, 0]
+        ss[:, :, 1, 1] = (alpha * sig) ** 2
+
+        return None, ss
