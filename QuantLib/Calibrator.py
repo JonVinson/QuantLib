48f27228df69728eb0dce9f10a0915a7a3583ca8
Jon Vinson <jdv@jdvinson.com>
2019-11-13 20:45:43 -0500

    QL-3 Finished calibrate function

--- QuantLib/Calibrator.py
+++ QuantLib/Calibrator.py
@@ -4,17 +4,19 @@ from scipy.interpolate import interp1d
 from FDSolver import FDSolver2D
 from FDDiffusionModels import DiffusionModel
 
-def calibrate(self, model, initParams, pBounds, bounds, n, T, nt, diffStart, knownDist, xDist):
+def calibrate(self, model, initParams, pBounds, bounds, n, T, nt, diffStart, knownDist, xDist, pIndex = None):
 
     [nx, ny] = n
 
     [a, b] = bounds[:2]
     dx = (b - a) / (nx - 1)
+    ix = int((diffStart[0] - a) / dx)
+
     [a, b] = bounds[2:]
     dy = (b - a) / (ny - 1)
-    dt = T / nt
+    iy = int((diffStart[1] - a) / dy)
 
-    [ix, iy] = diffStart
+    dt = T / nt
 
     solver = FDSolver2D(nx, dx, ny, dy, nt, dt)
     solver.cond = np.empty((nx, ny))
@@ -23,8 +25,21 @@ def calibrate(self, model, initParams, pBounds, bounds, n, T, nt, diffStart, kno
 
     knownDist = knownDist[:, np.newaxis]
 
+    npar = len(initParams)
+    q = np.zeros(npar)
+
+    if pIndex is None:
+        pIndex = range(npar)
+
+    nidx = len(pIndex) 
+    pComp = set(range(npar)) - set(pIndex)
+
     def opt_fun(p):
-        [solver.mu, solver.ss] = model.Calculate(p)
+        for i in range(nidx):
+            q[pIndex[i]] = p[i]
+        for i in range(npar - nidx):
+            q[pComp[i]] = initParams[i]
+        [solver.mu, solver.ss] = model.Calculate(q)
         solver.cond[:] = 0
         solver.cond[ix, iy] = 1
         solver.a[:] = 0
@@ -37,4 +52,9 @@ def calibrate(self, model, initParams, pBounds, bounds, n, T, nt, diffStart, kno
         dist = G / np.sum(G)
         return -np.sum(knownDist * np.log(dist))
 
-    return minimize(opt_fun, initParams, pBounds)
+    p0 = np.zeros(nidx)
+    
+    for i in range(nidx):
+        p0[i] = initParams[pIndex[i]]
+
+    return minimize(opt_fun, p0, pBounds)
