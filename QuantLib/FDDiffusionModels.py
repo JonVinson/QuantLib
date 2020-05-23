0cc96be68b50ec41a53ad275c56f421e5058d947
Jon Vinson <jdv@jdvinson.com>
2020-05-23 14:46:24 -0400

    QLB-1 Defined HestonModel class

--- QuantLib/FDDiffusionModels.py
+++ QuantLib/FDDiffusionModels.py
@@ -48,3 +48,46 @@ class SABRModel(DiffusionModel):
 
     def ParameterCount(self):
         return 3
+
+class HestonModel(DiffusionModel):
+
+    def __init__(self, bounds = None, n_points = None):
+        super().__init__(bounds, n_points)
+        self._S = None
+        self._m = None
+        self._ss = None
+
+    def Calculate(self, p):
+
+        [mu, theta, kappa, xi, rho] = p
+ 
+        if self._S is None:
+            self._S = self._x[:, np.newaxis]
+
+        S = self._S
+        nu = self._y
+        nx = self._nx
+        ny = self._ny
+
+        if self._m is None:
+            self._m = np.zeros((nx, ny, 2))  
+
+        m = self._m
+    
+        m[:, :, 0] = mu * S;
+        m[:, :, 1] = kappa * (theta - nu)
+
+        if self._ss is None:
+            self._ss = np.zeros((nx, ny, 2, 2))  
+
+        ss = self._ss
+    
+        ss[:, :, 0, 0] = nu * S ** 2
+        ss[:, :, 1, 0] = xi * rho * nu * S;
+        ss[:, :, 0, 1] = ss[:, :, 1, 0]
+        ss[:, :, 1, 1] = nu * xi ** 2;
+
+        return m, ss
+
+    def ParameterCount(self):
+        return 5
